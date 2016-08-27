995b3627f35050434489600ea90d6fca2370af3c
Eric Arnebäck <arnebackeric@gmail.com>
2016-08-27 20:09:30 +0200

    design project list.

--- create_index.py
+++ create_index.py
@@ -5,6 +5,56 @@ with open('src/template.html', 'r') as f:
 with open('src/index.html', 'r') as f:
     index_src=f.read()
 
+
+s = """
+  <div class="row gallery-row">
+    <div class="one-third column">
+      <img src="/img/gallery/deferred_shading.png" alt="img" >
+    </div>
+    <div class="one-third column">
+      <img src="/img/gallery/deferred_shading.png" alt="img" >
+    </div>
+    <div class="one-third column">
+      <img src="/img/gallery/deferred_shading.png" alt="img" >
+    </div>
+  </div>
+"""
+
+# extract all the rows from the file.
+with open('src/img_list.txt', 'r') as f:
+    rows = []
+    row = []
+    for line in f.readlines():
+        l = line.split()
+        img = l[0]
+        href = l[1]
+
+        if len(row) == 3:
+            rows.append(row)
+            row = []
+
+        row.append([img, href])
+    if len(row) > 0: # add last row
+        rows.append(row)
+
+img_list_src = ""
+
+# now format the rows as html
+for row in rows:
+    row_src = ""
+    row_src += '<div class="row gallery-row">'
+    for e in row:
+        row_src += """
+        <div class="one-third column">
+        <a href="{link}"><img src="{img}" alt="img" ></a>
+        </div>
+        """.format(img=e[0], link=e[1])
+
+    row_src += '</div>'
+    img_list_src = row_src + img_list_src
+
+index_src = index_src.format(img_list=img_list_src)
+
 src = template.format(src=index_src)
 
 with open('index.html', 'w') as f:
--- create_projects.py
+++ create_projects.py
@@ -1,11 +1,52 @@
+import xml.etree.ElementTree as ET
 
 with open('src/template.html', 'r') as f:
     template=f.read()
 
 with open('src/projects.html', 'r') as f:
-    projects_src=f.read()
+    project_src=f.read()
 
-src = template.format(src=projects_src)
+project_list = ""
+
+def elem2str(elem):
+    elem.text + ''.join(ET.tostring(e) for e in elem)
+
+
+    # print elem
+
+    # s = ''
+    # for c in elem.child:
+    #     s += ET.tostring(c)
+    # return s
+
+
+# parse the list of project from an xml file, and
+# then format as html.
+tree = ET.parse('src/project_list.xml')
+root = tree.getroot()
+#print ET.tostring(root)
+for child in root:
+    title = child[0].text
+    href = child[1].text
+    img = child[2].text
+    text = ET.tostring(child[3])[6:-11]
+
+    project_list += """
+    <hr>
+  <div class="row project-row">
+    <div class="two-thirds column">
+      <h2><a href="{href}">{title}</a></h2>
+      {text}
+    </div>
+    <div class="one-third column">
+      <a href="{href}"><img src="{img}" alt="img"></a>
+    </div>
+  </div>
+    """.format(href=href, text=text, title=title, img=img)
+
+project_src = project_src.format(project_list = project_list)
+
+src = template.format(src=project_src)
 
 with open('projects.html', 'w') as f:
     f.write(src)
