b435e4a3b244dc06529418736ccbc7b645cd92b5
Eric Arnebäck <arnebackeric@gmail.com>
2016-08-28 15:13:02 +0200

    Fixed better linking.

--- create_projects.py
+++ create_projects.py
@@ -11,15 +11,6 @@ project_list = ""
 def elem2str(elem):
     elem.text + ''.join(ET.tostring(e) for e in elem)
 
-
-    # print elem
-
-    # s = ''
-    # for c in elem.child:
-    #     s += ET.tostring(c)
-    # return s
-
-
 # parse the list of project from an xml file, and
 # then format as html.
 tree = ET.parse('src/project_list.xml')
@@ -31,20 +22,21 @@ for child in root:
     href = child[1].text
     img = child[2].text
     text = ET.tostring(child[3])[6:-11]
+    my_id = title.strip().replace(' ', '-').lower()
 
     project_list += "<hr>"
 
     project_list += """
   <div class="row project-row">
     <div class="two-thirds column">
-      <h2><a href="{href}">{title}</a></h2>
+      <h2 id="{my_id}"><a href="{href}">{title}</a></h2>
       {text}
     </div>
     <div class="one-third column">
       <a href="{href}"><img src="{img}" alt="img"></a>
     </div>
   </div>
-    """.format(href=href, text=text, title=title, img=img)
+    """.format(href=href, text=text, title=title, img=img, my_id=my_id)
 
 project_src = project_src.format(project_list = project_list)
 
